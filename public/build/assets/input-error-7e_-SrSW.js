cff04ffa73c0ffbf2c6f4fe46d531d8366592738
Mario Igkiempor <mario.igkiempor@theodo.com>
2025-06-10 09:24:26 +0100

    fix: push build assets

---
+++ public/build/assets/input-error-7e_-SrSW.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-6tQaYA4F.js";import{a as o}from"./app-logo-icon-1-HHu1lJ.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
